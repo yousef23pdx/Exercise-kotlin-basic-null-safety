e0019961238c83076ff3702e1f807d0e87b1eec6
Yousef Alothman <yousefoalothman@gmail.com>
2025-02-18 16:29:07 +0300

    Exercise Completed

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,29 @@
+fun evaluateScore(score: Double?):String {
+    val finalScore:Double = score ?: 0.0
+    return "The evaluated score = $finalScore"
+}
+
 fun main() {
-    println("Hello World!")
+    var number1:Int? = 10
+     number1 = null
+
+    println(number1)
+
+    var cityName:String? = "Kuwait City"
+    cityName = null
+
+    println(cityName)
+
+    var cityLength= cityName?.length
+    println(cityLength)
+
+    var cityLengthTest:Int? = cityName?.length ?: 10
+    println(cityLengthTest)
+
+    cityLengthTest = cityName?.length ?: 20
+    println(cityLengthTest)
+
+    println(evaluateScore(85.5))
+    println(evaluateScore(null))
+
 }
